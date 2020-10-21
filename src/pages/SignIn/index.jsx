45738be653e4891597caef2d9a0ff0f1a298757f
Wilson Santos <contato@wilsonsantos.net>
2020-10-21 19:06:02 -0300

    Design SignIn page component

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
+import { TextField, Button } from '@material-ui/core';
 import { authContext } from '../../contexts/auth';
 
 const SignIn = () => {
@@ -23,7 +24,31 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
-      <button type="button" onClick={handleSignIn}>Fazer Login</button>
+      <TextField
+        type="text"
+        variant="outlined"
+        label="Usuário"
+        name="username"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        type="password"
+        variant="outlined"
+        label="Senha"
+        name="password"
+        fullWidth
+        margin="normal"
+      />
+      <Button
+        type="button"
+        onClick={handleSignIn}
+        fullWidth
+        margin="normal"
+        color="primary"
+      >
+        Fazer Login
+      </Button>
     </div>
   );
 };
