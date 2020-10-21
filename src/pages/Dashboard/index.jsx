17930e21b2de8393858b8d9d0ebbdb1ec662ae79
Wilson Santos <contato@wilsonsantos.net>
2020-10-21 18:53:50 -0300

    Finish Dashboard Layout

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,20 +1,32 @@
-import React, { useContext } from 'react';
-import { } from '@material-ui/core';
-import { authContext } from '../../contexts/auth';
+import React from 'react';
+import { Avatar, makeStyles } from '@material-ui/core';
+import { deepPurple } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+    width: 120,
+    height: 120,
+  },
+}));
 
 const Dashboard = () => {
-  const { signOut } = useContext(authContext);
-
-  function handleLogout() {
-    signOut();
-  }
+  const classes = useStyles();
 
   return (
     <>
       <h1>Dashboard Page</h1>
-      <button type="button" onClick={handleLogout}>
-        Sair
-      </button>
+      <Avatar className={classes.purple}>WS</Avatar>
     </>
 
   );
