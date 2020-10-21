319c19a41372d5c81c6ed8b52ecd58cf036dc506
Wilson Santos <contato@wilsonsantos.net>
2020-10-21 16:26:41 -0300

    Add PrivateRoute component

---
+++ src/routes/PrivateRoute/index.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { Redirect, Route, useLocation } from 'react-router-dom';
+
+import { authContext } from '../../contexts/auth';
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { signed } = useContext(authContext);
+  return (
+
+    <Route
+      {...rest}
+      render={({ location }) => (signed ? (
+        children
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: location },
+          }}
+        />
+      ))}
+    />
+  );
+};
+
+export default PrivateRoute;
